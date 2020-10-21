1dcd91b00fca91013a72ee46cbc74f26fc4650b4
aaroeni <72748341+aaroeni@users.noreply.github.com>
2020-10-21 17:05:30 +0200

    Add files via upload

---
+++ ArticleRating/pages.py
@@ -0,0 +1,17 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+
+class ExplanationArticleRatingPage1(Page):
+    pass
+
+class ExplanationArticleRatingPage2(Page):
+    pass
+
+
+
+
+page_sequence = [ExplanationArticleRatingPage1,
+                 ExplanationArticleRatingPage2]
